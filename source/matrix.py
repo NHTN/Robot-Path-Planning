bcdd1daa6de1a63f40f29ab153c047d1758f8f67
NHTNhi <1712117@student.hcmus.edu.vn>
2019-10-03 14:49:15 +0700

    -update bfsPath-

--- source/matrix.py
+++ source/matrix.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 
 borderLimit = [0, 0]
 start = [0, 0]
@@ -16,107 +18,117 @@ f.close()
 print(polys)
 
 
-mark = []
-for i in range(100):
-    mark.append(0)
-
-# Check if a point is inside a polygon
-def onSegment(a, b, c): 
-    return (b[0] <= max(a[0], c[0]) and b[0] >= min(a[0], c[0]) and b[1] <= max(a[1], c[1]) and b[1] >= min(a[1], c[1]))
-
-def orientation(a, b, c):
-    val = (b[1] - a[1]) * (c[0] - b[0]) - (b[0] - a[0]) * (c[1] - b[1])
-    if (val == 0): return 0
-    if (val > 0): return 1
-    return 2
-
-def doIntersect(p1, q1, p2, q2):
-    case1 = orientation(p1, q1, p2)
-    case2 = orientation(p1, q1, q2)
-    case3 = orientation(p2, q2, p1)
-    case4 = orientation(p2, q2, q1)
-
-    if (case1 != case2 and case3 != case4): return True
-
-    if (case1 == 0 and onSegment(p1, p2, q1)): 
-        return True
-    if (case2 == 0 and onSegment(p1, q2, q1)): 
-        return True
-    if (case3 == 0 and onSegment(p2, p1, q2)): 
-        return True
-    if (case4 == 0 and onSegment(p2, q1, q2)): 
-        return True
-
-    return False
-
-def isInside(polygon, nEdge, checkPoint):
-    if (nEdge < 3 ): 
-        return False
-    count = 0
-    i = 0
-
-    extreme = (10000, checkPoint[1])
-    while True:
-        next = (i+1)%nEdge
-        if (doIntersect(polygon[i+1], polygon[next+1], checkPoint, extreme)):
-            if (orientation(polygon[i+1], checkPoint, polygon[next+1]) == 0):
-                return onSegment(polygon[i+1], checkPoint, polygon[next+1])
-            count += 1
-        i = next
-        if (i == 0): break
-
-   # print((i+1)%nEdge)
-    return count&1 #count%2 == 1
-
-def getDistance(p, q):
-    return abs(p[0]-q[0]) + abs(p[1]-q[1])
-
-
 def getPos(point):
     return ((point[0]-1)*borderLimit[0]+point[1])
 
-def dfsPath(start, goal, path):
-    path.append(start)
-    if (getPos(start) == getPos(goal)): 
-        return True
+visited = []
+for i in range(0, (borderLimit[0]+2)*(borderLimit[1]+2)):
+    visited.append(0)
+pre = []
+for i in range(0, borderLimit[0]+1):
+    pre += [[(0, 0)] * (borderLimit[1]+1)]
 
-    nextStep = []
-    del nextStep[:]
-    if (start[0] > 1):
-        nextStep.append(((start[0]-1, start[1]), 1))
-    if (start[0] < borderLimit[0]-1):
-        nextStep.append(((start[0]+1, start[1]), 1)) 
-    if (start[1] > 1):
-        nextStep.append(((start[0], start[1]-1), 1))
-    if (start[1] < borderLimit[1]-1):
-        nextStep.append(((start[0], start[1]+1), 1))
+def bfsPath(start, goal):
+    for i in range(0, borderLimit[1]):
+        canMoveTo[0][i] = 4
+        canMoveTo[borderLimit[0]][i] = 4
 
-    for next in range(0, len(nextStep)):
-        v = getPos(nextStep[next][0])
-        if ((mark[v] == 0)):
-            mark[v] = 1
-            dfsPath(nextStep[next][0], goal, path)
-            
-path = []
-mark[getPos(start)] = 1
-dfsPath(start, goal, path)
-print(path)
+    for j in range(0, borderLimit[0]):
+        canMoveTo[j][0] = 4
+        canMoveTo[j][borderLimit[1]] = 4
+    canMoveTo[borderLimit[0]][borderLimit[1]] = 4
+    
+    queue = []
+    queue.append([0, start])
+    canMoveTo[start[0]][start[1]] = 5
+
+    while queue:
+        du, u = queue.pop(0)
+        if all(visited):
+            break
+        visited[getPos(u)] = 1
+        
+        if (u[0] > 1  and canMoveTo[u[0]-1][u[1]] == 1):
+            canMoveTo[u[0]-1][u[1]] = 5
+            pre[u[0]-1][u[1]] = u
+            queue += [[du+1, [u[0]-1, u[1]]]]
+
+        if (u[0] < borderLimit[0]-1  and canMoveTo[u[0]+1][u[1]] == 1):
+            canMoveTo[u[0]+1][u[1]] = 5
+            pre[u[0]+1][u[1]] = u
+            queue += [[du+1, [u[0]+1, u[1]]]]
+
+        if (u[1] > 1 and canMoveTo[u[0]][u[1]-1] == 1):
+            canMoveTo[u[0]][u[1]-1] = 5
+            pre[u[0]][u[1]-1] = u
+            queue += [[du+1, [u[0], u[1]-1]]]
+
+        if (u[1] < borderLimit[1]-1 and canMoveTo[u[0]][u[1]+1] == 1):
+            canMoveTo[u[0]][u[1]+1] = 5
+            pre[u[0]][u[1]+1] = u
+            queue += [[du+1, [u[0], u[1]+1]]]
+
+
+polyList = []
+for i in range(0, polyCount):
+    temp = []
+    for j in range(0, int(len(polys[i])/2)):
+        temp.append((polys[i][j*2], polys[i][j*2+1]))
+    polyList.append(temp)
 
 canMoveTo = []
-for i in range(1, (borderLimit[0]+1)*(borderLimit[1]+1)):
-    canMoveTo.append(True)
+for i in range(0, borderLimit[0]+1):
+    canMoveTo += [[1] * (borderLimit[1]+1)]
 
-for xCoordinate in range(1, borderLimit[0]+1):
-    for yCoordinate in range(1, borderLimit[1]+1):
-        tempPoint = (xCoordinate, yCoordinate)
-        for i in range(0, polyCount):
-            print(polys[i]), 
-            print(xCoordinate), 
-            print(yCoordinate)
-            '''
-            if (isInside(polys[i], polys[i][0], tempPoint)):
-                print(getPos(tempPoint))
-                canMoveTo[getPos(tempPoint)] = False'''
-        
-print(canMoveTo)
-print(isInside(polys[0], polys[0][0], (1, 1)))
+
+def bresehamLine(p0, p1):
+    x0, y0 = p0
+    x1, y1 = p1
+
+    dx = abs(x1-x0)
+    dy = abs(y1-y0)
+    if x0 < x1:
+        sx = 1
+    else:
+        sx = -1
+
+    if y0 < y1:
+        sy = 1
+    else:
+        sy = -1
+    err = dx-dy
+    while True:
+        canMoveTo[x0][y0] = 0
+        if x0 == x1 and y0 == y1:
+            return
+        e2 = 2*err
+        if e2 > -dy:
+            err = err - dy
+            x0 = x0 + sx
+        if e2 < dx:
+            err = err + dx
+            y0 = y0 + sy
+
+def drawPolygon():
+    for i in range(0, polyCount):
+        for j in range(0, len(polyList[i])):
+            if (j == len(polyList[i])-1):
+                bresehamLine(polyList[i][j], polyList[i][0])
+            else:
+                bresehamLine(polyList[i][j], polyList[i][j+1])
+
+
+drawPolygon()
+bfsPath(start, goal)
+
+temp = pre[goal[0]][goal[1]]
+while (temp != start):
+    canMoveTo[temp[0]][temp[1]] = 8
+    temp = pre[temp[0]][temp[1]]
+
+
+canMoveTo[start[0]][start[1]] = 2
+canMoveTo[goal[0]][goal[1]] = 3
+plt.matshow(canMoveTo)
+plt.show()
+
